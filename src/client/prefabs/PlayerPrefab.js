4abc6e7ee4a7f5bde6822a5bb41051b7d73b2fe2
Chris Sheppard <chris.sheppard@thebioagency.com>
2016-07-18 16:23:55 +0100

    Refactoring movements

--- src/client/prefabs/PlayerPrefab.js
+++ src/client/prefabs/PlayerPrefab.js
@@ -3,10 +3,11 @@ import Prefab from "./Prefab";
 
 const WALK_SLIDE_BUFFER = 10;
 const WALL_SLIDE_SPEED = 100;
-const WALL_JUMP_SIDE_ACCELERATION = 1000;
-const WALL_JUMP_SIDE_VELOCITY = 250;
 const JUMP_VARIABLE_HEIGHT = -120;
 const JUMP_ACCELERATION = -35000;
+const ACCELERATION_X = 150;
+const ACCELERATION_X_AIR = 2;
+const MIN_MOVEMENT_INPUT = 0.25;
 
 
 export default class PlayerPrefab extends Prefab {
@@ -30,9 +31,6 @@ export default class PlayerPrefab extends Prefab {
         this.state.game.physics.arcade.enable(this);
         // this.body.collideWorldBounds = true;
 
-        this.walkingSpeed = 150;
-        this.jumpingSpeed = 500;
-
         this.body.drag.x = 900;
         this.body.maxVelocity.x = 250;
         this.body.maxVelocity.y = 1000;
@@ -46,9 +44,9 @@ export default class PlayerPrefab extends Prefab {
         this.puffs.createMultiple(5, "puff", 0, false);
         this.puffs.alpha = 0.5;
         this.puffs.callAll("animations.add", "animations", "puff", [0, 1, 2, 3, 4], 20, true);
-        this.puffs.setAll('anchor.x', 0.5);
-        this.puffs.setAll('scale.x', 0.5);
-        this.puffs.setAll('scale.y', 0.5);
+        this.puffs.setAll("anchor.x", 0.5);
+        this.puffs.setAll("scale.x", 0.5);
+        this.puffs.setAll("scale.y", 0.5);
     }
 
     faceDirection(direction) {
@@ -62,73 +60,8 @@ export default class PlayerPrefab extends Prefab {
         this.state.game.physics.arcade.collide(this, this.state.layers.collision);
         this.state.game.physics.arcade.collide(this, this.state.groups.collision);
 
-        this.updateWalking();
-        this.updateJumping();
-        this.updateWallSliding();
-        this.updateWallJumping();
-
-        // TODO: Double jump
-
-        this.game.world.wrap(this, 0, true);
-    }
-
-    updateWalking() {
-        // Move left
-        if (this.cursors.left.isDown) {
-            if (this.body.velocity.x > WALK_SLIDE_BUFFER) {
-                this.body.velocity.x = WALK_SLIDE_BUFFER;
-            }
-
-            this.body.acceleration.x -= this.walkingSpeed;
-
-            this.faceDirection("left");
-        } else if (this.cursors.left.justUp) {
-            this.body.acceleration.x = 0;
-        }
-
-        // Move right
-        if (this.cursors.right.isDown) {
-            if (this.body.velocity.x < -WALK_SLIDE_BUFFER) {
-                this.body.velocity.x = -WALK_SLIDE_BUFFER;
-            }
-
-            this.body.acceleration.x += this.walkingSpeed;
-
-            this.faceDirection("right");
-        } else if (this.cursors.right.justUp) {
-            this.body.acceleration.x = 0;
-        }
-
-        if (Math.abs(this.body.velocity.x) > WALK_SLIDE_BUFFER) {
-            // Change walking animation speed?
-            //this.animations.currentAnim.speed = 20
-            this.animations.play("walk");
-        } else if (Math.abs(this.body.velocity.y) < WALK_SLIDE_BUFFER) {
-            this.animations.play("idle");
-        }
-    }
-
-    updateJumping() {
         this.body.acceleration.y = 0;
 
-        // Jump only if touching a tile
-        if (
-            this.cursors.up.isDown && !this.cursors.up.repeats &&
-            (this.body.blocked.down || this.body.touching.down)
-        ) {
-            this.body.acceleration.y = JUMP_ACCELERATION;
-            this.animations.play("jump");
-
-            this.showPuff();
-        }
-
-        // Variable height jump
-        if (this.cursors.up.isUp && this.cursors.up.justDown && this.body.velocity.y < JUMP_VARIABLE_HEIGHT) {
-            this.body.velocity.y = JUMP_VARIABLE_HEIGHT;
-        }
-    }
-
-    updateWallSliding() {
         // Wall sliding
         if (!this.body.blocked.down && !this.body.touching.down) {
             if (this.cursors.right.isDown && (this.body.blocked.right || this.body.touching.right)) {
@@ -136,39 +69,115 @@ export default class PlayerPrefab extends Prefab {
                     this.frameName = "Jump__003.png";
                     this.body.velocity.y = WALL_SLIDE_SPEED;
                 }
-            }
 
-            if (this.cursors.left.isDown && (this.body.blocked.left || this.body.touching.left)) {
+                this.faceDirection("right");
+            } else if (this.cursors.left.isDown && (this.body.blocked.left || this.body.touching.left)) {
                 if (this.body.velocity.y > WALL_SLIDE_SPEED) {
                     this.frameName = "Jump__003.png";
                     this.body.velocity.y = WALL_SLIDE_SPEED;
                 }
 
                 this.faceDirection("left");
+            } else {
+                this.killAccelerationIfHittingWall();
             }
+        } else {
+            this.killAccelerationIfHittingWall();
         }
-    }
 
-    updateWallJumping() {
-        // TODO: Possibly refactor to use the same jumping calculation as static jumps
+        // Move right
+        if (this.cursors.right.isDown) {
+            this.updateMoveSpeed(1);
+        } else if (this.cursors.right.justUp && !this.body.blocked.right) {
+            this.body.acceleration.x = 0;
+        }
+
+        // Move left
+        if (this.cursors.left.isDown) {
+            this.updateMoveSpeed(-1);
+        } else if (this.cursors.left.justUp && !this.body.blocked.left) {
+            this.body.acceleration.x = 0;
+        }
+
+        // Animations
+        if (Math.abs(this.body.velocity.x) > WALK_SLIDE_BUFFER) {
+            // Change walking animation speed?
+            //this.animations.currentAnim.speed = 20
+            this.animations.play("walk");
+        } else if (Math.abs(this.body.velocity.y) < WALK_SLIDE_BUFFER) {
+            this.animations.play("idle");
+        }
+
+        // Jumping
+        if (
+            this.cursors.up.isDown && !this.cursors.up.repeats &&
+            (this.body.blocked.down || this.body.touching.down)
+        ) {
+            this.jump();
+        }
+
         // Wall jumping
         if (!this.body.blocked.down && !this.body.touching.down) {
             if ((this.body.blocked.right || this.body.touching.right) && this.cursors.up.isDown && !this.cursors.up.repeats) {
-                this.body.velocity.y = -this.jumpingSpeed;
-                this.body.acceleration.x = -WALL_JUMP_SIDE_ACCELERATION;
-                this.body.velocity.x = -WALL_JUMP_SIDE_VELOCITY;
-                this.animations.play("jump");
-                this.faceDirection("left");
-                this.showPuff();
+                this.updateMoveSpeed(-1);
+                this.jump();
             } else if ((this.body.blocked.left || this.body.touching.left) && this.cursors.up.isDown && !this.cursors.up.repeats) {
-                this.body.velocity.y = -this.jumpingSpeed;
-                this.body.acceleration.x = WALL_JUMP_SIDE_ACCELERATION;
-                this.body.velocity.x = WALL_JUMP_SIDE_VELOCITY;
-                this.animations.play("jump");
-                this.faceDirection("right");
-                this.showPuff();
+                this.updateMoveSpeed(1);
+                this.jump();
             }
         }
+
+        // Variable height jump
+        if (this.cursors.up.isUp && this.cursors.up.justDown && this.body.velocity.y < JUMP_VARIABLE_HEIGHT) {
+            this.body.velocity.y = JUMP_VARIABLE_HEIGHT;
+        }
+
+        // TODO: Double jump
+
+        this.game.world.wrap(this, 0, true);
+    }
+
+    jump() {
+        if (this.body.velocity.y > 0) {
+            this.body.velocity.y = 0;
+        }
+
+        this.body.acceleration.y = JUMP_ACCELERATION;
+        this.animations.play("jump");
+        this.showPuff();
+    }
+
+    killAccelerationIfHittingWall() {
+        // Kill acceleration when hitting a wall
+        if (
+            (this.body.acceleration.x > 0 && (this.body.blocked.right || this.body.touching.right)) ||
+            (this.body.acceleration.x < 0 && (this.body.blocked.left || this.body.touching.left))
+        ) {
+            this.body.acceleration.x = 0;
+        }
+    }
+
+    updateMoveSpeed(speed) {
+        // Ignore if the player input speed is too slow
+        if (speed * speed < MIN_MOVEMENT_INPUT) {
+            speed = 0;
+        }
+
+        // Allow fast turning
+        if ((speed > 0) != (this.body.velocity.x > 0) && this.body.velocity.x != 0) {
+            this.body.velocity.x = 0;
+            this.body.acceleration.x = 0;
+        }
+
+        // Update acceleration
+        if (this.body.blocked.down || this.body.touching.down) {
+            this.body.acceleration.x += ACCELERATION_X * speed;
+        } else {
+            this.body.acceleration.x += ACCELERATION_X * speed * ACCELERATION_X_AIR;
+        }
+
+        // Update facing direction
+        this.faceDirection(speed > 0 ? "right" : "left");
     }
 
     showPuff() {
